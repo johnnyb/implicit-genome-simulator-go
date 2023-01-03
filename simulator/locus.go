267e09d92f8ca7b2431d8c0e7eb08d233d8845f6
Jonathan Bartlett <jonathan@bartlettpublishing.com>
2023-01-02 18:11:20 -0600

    Report whether or not a locus was actually mutated

--- simulator/locus.go
+++ simulator/locus.go
@@ -3,7 +3,7 @@ package simulator
 // Locus is the real locus within an Organism, based on an ImplicitLocus (which defines the possible range and mutability).
 type Locus struct {
 	ImplicitLocus *ImplicitLocus
-	Value float32
+	Value         float32
 }
 
 // Generates a new locus based on an implicit locus and generates a random initial value for it (within the proper range).
@@ -11,23 +11,25 @@ func NewLocus(ilocus *ImplicitLocus) Locus {
 	if ilocus == nil {
 		panic("Nil ilocus")
 	}
-	l := Locus {
+	l := Locus{
 		ImplicitLocus: ilocus,
-		Value: ilocus.GenerateValue(),
+		Value:         ilocus.GenerateValue(),
 	}
 
 	return l
 }
 
 // Determines, based on the mutation rate, if we should mutate this locus.
-func (rec *Locus) PossiblyMutate() {
+func (rec *Locus) PossiblyMutate() bool {
 	if RandomFloat() < rec.ImplicitLocus.Mutability {
 		rec.Mutate()
+		return true
 	}
+
+	return false
 }
 
 // Generates a new value for this locus, potentially based on the previous value.
 func (rec *Locus) Mutate() {
 	rec.Value = rec.ImplicitLocus.GenerateModifiedValueFrom(rec.Value)
 }
-
